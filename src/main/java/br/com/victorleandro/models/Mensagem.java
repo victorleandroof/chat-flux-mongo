03440ad05bc837078e3f28131cd9f9b110ecfc36
v.leandro <v.leandro@samsung.com>
2020-03-12 12:39:54 -0300

    resolve tailable

--- src/main/java/br/com/victorleandro/models/Mensagem.java
+++ src/main/java/br/com/victorleandro/models/Mensagem.java
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-@Document(collection = "mensagens")
+@Document(collection = "teste")
 public class Mensagem {
 
 	@Id
